9542255bf99a4fa8d5fb28ab31967d1bd2f99806
Marouen JENDOUBI <jendoubim@asfa-auto.fr>
2018-02-22 17:45:28 +0100

    front controller

---
+++ core/FrontControllerInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jendom
+ * Date: 22/02/2018
+ * Time: 17:43
+ */
+
+namespace Tripsorter\Core;
+
+
+interface FrontControllerInterface
+{
+    public function setController($controller);
+    public function setAction($action);
+    public function setParams(array $params);
+    public function run();
+}
